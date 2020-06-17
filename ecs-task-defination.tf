41bcf763575f8625f77c65a79322e8b7e8467cb9
vishal <vishal.bhogate07@gmail.com>
2020-06-17 19:41:51 +1000

    Remobving extra variables

--- ecs-task-defination.tf
+++ ecs-task-defination.tf
@@ -2,5 +2,5 @@ resource aws_ecs_task_definition "ecs" {
   family                = var.name
   execution_role_arn    = aws_iam_role.ecs.arn
   task_role_arn         = try(var.task_role_arn, null)
-  container_definitions = var.container_definitions
+  container_definitions = jsonencode(var.container_definitions)
 }
