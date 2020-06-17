7399d3807bd4c795f41bd261fc60d6a20b9ff388
vishal <vishal.bhogate07@gmail.com>
2020-06-17 13:55:25 +1000

    ecs-schedule-task

--- _data.tf
+++ _data.tf
@@ -1,1 +1,11 @@
-data "type" "name" {}
+data aws_iam_policy_document "ecs_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    sid = ""
+  }
+}
---
+++ cloudwatch-event-rule.tf
@@ -0,0 +1,33 @@
+resource aws_cloudwatch_event_rule "ecs" {
+  name                = "ecs-schedule-task-${var.name}"
+  description         = "ECS Schedule Task"
+  schedule_expression = var.schedule_expression
+  is_enabled          = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_cloudwatch_event_target "ecs" {
+  target_id = "run-scheduled-id-${var.name}"
+  arn       = var.cluster_arn
+  rule      = aws_cloudwatch_event_rule.ecs.name
+  role_arn  = var.ecs_events_role_arn != "" ? join(",", var.ecs_events_role_arn, aws_iam_role.ecs.arn) : aws_iam_role.ecs.arn
+
+  ecs_target {
+    launch_type         = var.launch_type
+    task_count          = var.task_count
+    task_definition_arn = var.task_definition_arn
+
+    network_configuration {
+      subnets          = var.private_subnets
+      security_groups  = var.security_groups
+      assign_public_ip = var.assign_public_ip
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
