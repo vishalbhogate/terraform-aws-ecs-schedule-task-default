c504b553a417208b2c997b88856cfa1c41217d1a
vishal <vishal.bhogate07@gmail.com>
2020-07-09 12:58:42 +1000

    Fixing typo

--- ecs-task-defination.tf
+++ ecs-task-defination.tf
@@ -3,7 +3,7 @@ resource aws_ecs_task_definition "ecs" {
   execution_role_arn       = aws_iam_role.ecs_task_excution.arn
   task_role_arn            = try(var.task_role_arn, null)
   network_mode             = var.network_mode
-  cpu                      = var.cpu : null
+  cpu                      = var.cpu
   requires_compatibilities = var.requires_compatibilities
   container_definitions    = <<TASK_DEFINITION
 [
