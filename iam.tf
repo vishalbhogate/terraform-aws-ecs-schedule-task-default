386f0b627994792c41e69b71c0da283c36f59045
Vishal Bhogate <vishal.bhogate@brighte.com.au>
2020-08-18 17:06:23 +1000

    Adding SSM Policy

--- iam.tf
+++ iam.tf
@@ -21,3 +21,8 @@ resource aws_iam_role_policy_attachment "ecs_task_policy" {
   role       = aws_iam_role.ecs_task_excution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource aws_iam_role_policy_attachment "ecs_task_policy_ssm" {
+  role       = aws_iam_role.ecs_task_excution.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
